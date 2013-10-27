db4ec3288531d0aa2568bb56ab2a980e0a66e8a9
Martin Prunell <martin.prunell@gmail.com>
2013-10-27 12:03:24 -0200

    2nd iteration

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,8 +3,9 @@
  */
 $(document).ready(function() {
     loadImages();
+    initMap();
     mapButton();
-})
+});
 
 function loadImages() {
     $.getJSON( '/images', function(images) {
@@ -12,10 +13,11 @@ function loadImages() {
     });
 }
 
+
 function startSlideShow(images) {
     var backgrounds = [];
     $(images).each(function(index, image) {
-        backgrounds.push({ src: '/images/' + image, fade: 1000 })
+        backgrounds.push({ src: '/images/' + image, fade: 1000 });
     });
     $.vegas('slideshow', {
         backgrounds: backgrounds
@@ -26,7 +28,23 @@ function startSlideShow(images) {
 
 function mapButton() {
     $('li.map').click(function(e) {
-        e.preventDefault();
-        document.location.href = 'map';
+        $('#map-window').reveal({
+            animation: 'fadeAndPop',
+            animationspeed: 300,
+            closeonbackgroundclick: true,
+            dismissmodalclass: 'close-reveal-modal'
+        });
     });
+}
+
+function initMap() {
+    map = new OpenLayers.Map('basic-map');
+    var mapnik         = new OpenLayers.Layer.OSM();
+    var fromProjection = new OpenLayers.Projection('EPSG:4326');   // Transform from WGS 1984
+    var toProjection   = new OpenLayers.Projection('EPSG:900913'); // to Spherical Mercator Projection
+    var position       = new OpenLayers.LonLat(-56.100311279296875, -34.89177445225364).transform( fromProjection, toProjection);
+    var zoom           = 14;
+
+    map.addLayer(mapnik);
+    map.setCenter(position, zoom );
 }
