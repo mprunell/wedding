809b51793875173540566fb32160c7d3f960e216
Martin Prunell <martin.prunell@gmail.com>
2013-10-31 20:01:48 -0200

    Change icons, font family, RSVP

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -60,20 +60,32 @@ function initMap() {
             new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
             map.getProjectionObject() // to Spherical Mercator Projection
         );
-    var size = new OpenLayers.Size(21,25);
+    var size = new OpenLayers.Size(42,50);
     var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
     var icon = new OpenLayers.Icon('/javascripts/images/cross.png', size, offset)
     markers.addMarker(new OpenLayers.Marker(lonLat1, icon));
+    var popup1 = new OpenLayers.Popup.FramedCloud("Popup",
+        lonLat1, null,
+        '<a target="_blank" href="http://openlayers.org/">We</a> ' +
+            'could be here.<br>Or elsewhere.', null,
+        true // <-- true if we want a close (X) button, false otherwise
+    );
 
     var lonLat2 = new OpenLayers.LonLat(-56.02457, -34.85253)
         .transform(
             new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
             map.getProjectionObject() // to Spherical Mercator Projection
         );
-    var size = new OpenLayers.Size(21,25);
+    var size = new OpenLayers.Size(42,50);
     var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
     var icon = new OpenLayers.Icon('/javascripts/images/party.png', size, offset)
     markers.addMarker(new OpenLayers.Marker(lonLat2, icon));
+    var popup2 = new OpenLayers.Popup.FramedCloud("Popup",
+        lonLat2, null,
+        '<a target="_blank" href="http://openlayers.org/">We</a> ' +
+            'could be here.<br>Or elsewhere.', null,
+        true // <-- true if we want a close (X) button, false otherwise
+    );
 
     var fromProjection = new OpenLayers.Projection('EPSG:4326');   // Transform from WGS 1984
     var toProjection   = new OpenLayers.Projection('EPSG:900913'); // to Spherical Mercator Projection
@@ -84,4 +96,6 @@ function initMap() {
         );
     var zoom = 13;
     map.setCenter (position, zoom);
+    map.addPopup(popup1);
+    map.addPopup(popup2);
 }
